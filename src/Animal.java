378bd1183adc21c2a5a673febfedcb5dd5588337
dz2244 <dz2244@bs.amalnet.k12.il>
2024-09-24 16:07:25 +0300

    commit 1

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    public static void move()
+    {
+     System.out.println("u can move");
+    }
+    public static void makeSound()
+    {
+        System.out.println("animal sound");
+
+    }
+
+}
---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends  Animal {
+    @Override
+    public static void move()
+    {
+        System.out.println("u can move");
+    }
+    @Override
+    public static void makeSound()
+    {
+        System.out.println("animal sound");
+
+    }
+
+}
